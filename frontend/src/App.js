f85ae15c2fb008a7fb40b046ee383b1308587679
Mehraz <mehrazhossain54@gmail.com>
2023-01-27 07:38:09 +0600

    Setup tailwind & basic components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1>Welcome to Monohor</h1>
+      <Header />
+      <main>
+        <h1>Welcome to Monohor</h1>
+      </main>
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <footer>Copyrights &copy; Monohor</footer>;
+};
+
+export default Footer;
