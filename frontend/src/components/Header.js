2879d08ae42f40727c85713dbc2b5e02b0d5feb3
Mehraz <mehrazhossain54@gmail.com>
2023-01-31 02:03:37 +0600

    added footer v2

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,11 +2,10 @@ import React from "react";
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import {
-  ChevronDownIcon,
   MagnifyingGlassIcon,
   ShoppingBagIcon,
 } from "@heroicons/react/20/solid";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const Header = () => {
   const user = {
